94a441368b7b8fa2ab50c6b23b4aa6a324b4771a
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-12-01 20:42:23 -0300

    style: change classes

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -5,20 +5,28 @@ import Consultorio from "../../../public/consultorio-01.png";
 
 const skills: { title: string; text: string }[] = [
   {
-    title: "Estética do Sorriso",
-    text: "Transformação e aprimoramento estético através de tratamentos ortodônticos especializados.",
-  },
-  {
     title: "Inovação Digital",
-    text: "Utilização de tecnologias digitais avançadas para planos de tratamento ortodôntico personalizados e precisos.",
+    text: "Utilização de tecnologias avançadas para criar planos de tratamento personalizados.",
   },
   {
     title: "Ortodontia Preventiva",
-    text: "Intervenções precoces para prevenir problemas ortodônticos e promover o desenvolvimento saudável.",
+    text: "Intervenções precoces para prevenir problemas ortodônticos.",
   },
   {
-    title: "Tratamento Invisível",
-    text: "Especialização em opções estéticas para correção ortodôntica de uma maneira discreta e confortável.",
+    title: "Tratamento Discreto",
+    text: "Especialização em opções estéticas para correção de forma discreta e confortável.",
+  },
+  {
+    title: "Cuidado Infantil",
+    text: "Atendimento especializado crianças, garantindo um sorriso saudável.",
+  },
+  {
+    title: "Harmonia Maxilar",
+    text: "Abordagem que visa precisamente corrigir desarmonias na posição dos maxilares.",
+  },
+  {
+    title: "Integração Cirúrgica",
+    text: "Colaboração com cirurgiões bucomaxilofaciais para casos cirúrgicos.",
   },
 ];
 
@@ -35,18 +43,23 @@ const SkillsSection = () => {
       />
 
       <div className="bg-[#F5F5F5] w-full h-fit flex flex-col items-center justify-center py-6">
-        <h2 className="font-medium text-xl">
+        <h2 className="font-semibold text-xl md:text-2xl">
           <span className="font-light">Especialidades da</span> Dra. Lauren
           Mitchell
         </h2>
 
-        <div className="my-8 grid grid-cols-1 md:grid-cols-2 gap-10 w-full md:max-w-[80%] mx-auto">
-            {skills.map((skill, index) => (
-                <div key={index} className="flex flex-col items-center justify-center text-center gap-2">
-                    <h3 className="font-bold text-laurenBlue text-xl">{skill.title}</h3>
-                    <p className="font-light text-sm w-full max-w-xs md:max-w-lg">{skill.text}</p>
-                </div>
-            ))}
+        <div className="my-8 grid grid-cols-1 md:grid-cols-2 gap-10 w-full max-w-[90%] lg:w-[60rem] mx-auto ">
+          {skills.map((skill, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center text-center gap-2 w-full max-w-xs md:max-w-none mx-auto"
+            >
+              <h3 className="font-bold text-laurenBlue text-xl">
+                {skill.title}
+              </h3>
+              <p className="font-light text-sm lg:px-20 ">{skill.text}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
